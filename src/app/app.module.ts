629359e14c6616fb0792f7c3737377e6182cd29a
karthy <karthickted@gmail.com>
2019-09-22 14:44:30 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HelloComponent } from './hello.component';
 import { HomeModule } from './home/home.module';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSidenavModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HomeModule, HttpClientModule, MatSidenavModule ],
+  imports:      [ BrowserModule, FormsModule, HomeModule, HttpClientModule, MatSidenavModule, BrowserAnimationsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
